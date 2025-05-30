91791b87843679fda2a8b454a57c87b6c035432b
Shivambhat12 <shivam4bhatnagar@gmail.com>
2025-05-30 19:42:45 +0530

    added major four functions

--- backend/router/auctionItemRoutes.js
+++ backend/router/auctionItemRoutes.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { addNewAuctionItem } from "../controllers/auctionItemController.js";
+import {
+  addNewAuctionItem,
+  getAllItems,
+  getAuctionDetails,
+  getMyAuctionItems,
+  removeFromAuction,
+} from "../controllers/auctionItemController.js";
 import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -10,4 +16,24 @@ router.post(
   addNewAuctionItem
 );
 
+router.get("/allitems", getAllItems);
+router.get("/auction/:id", isAuthenticated, getAuctionDetails);
+router.get(
+  "/myitems",
+  isAuthenticated,
+  isAuthorized("Auctioneer"),
+  getMyAuctionItems
+);
+router.delete(
+  "/delete/:id",
+  isAuthenticated,
+  isAuthorized("Auctioneer"),
+  removeFromAuction
+);
+router.put(
+  "/item/republish/:id",
+  isAuthenticated,
+  isAuthorized("Auctioneer"),
+  addNewAuctionItem
+);
 export default router;
