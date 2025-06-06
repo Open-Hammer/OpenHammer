0a73968fe4a2e01625c307dfd8bd5c4add487397
Inderjeet1Singh <inderjeet2023mnnit@gmail.com>
2025-06-06 22:33:24 +0530

    Adding Leaderboard, Featured Auctions,Today's Auctions Section in Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,15 +9,18 @@ import Login from "./pages/Login.jsx";
 import SubmitCommission from "./pages/SubmitCommission.jsx";
 import { useDispatch } from "react-redux";
 import Footer from "./layout/Footer.jsx";
-import { fetchUser } from "./store/slices/userSlice.js";
+import { fetchUser, fetchLeaderboard } from "./store/slices/userSlice.js";
 import AboutUs from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
 import { getAllAuctionItems } from "./store/slices/auctionSlice.js";
+import Leaderboard from "./layout/Leaderboard.jsx";
+import Auctions from "./pages/Auctions.jsx";
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchUser());
     dispatch(getAllAuctionItems());
+    dispatch(fetchLeaderboard());
   }, []);
   return (
     <Router>
@@ -29,6 +32,8 @@ const App = () => {
         <Route path="/submit-commission" element={<SubmitCommission />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/auctions" element={<Auctions />} />
       </Routes>
       <ToastContainer position="top-right" />
       <Footer />
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -11,7 +11,6 @@ import {
 import { Link } from "react-router-dom";
 
 const AboutUs = () => {
-  
   const features = [
     {
       icon: <FaGavel size={30} />,
